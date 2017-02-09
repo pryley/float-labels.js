096836da9c65e9d709196149b382d35f2a15d5ae
Paul Ryley <paul@geminilabs.io>
2017-02-09 11:24:44 -0400

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,20 @@
 var browserSync  = require( 'browser-sync' ).create();
 var gulp         = require( 'gulp' );
 var autoprefixer = require( 'gulp-autoprefixer' );
+var bump         = require( 'gulp-bump' );
 var cssnano      = require( 'gulp-cssnano' );
+var gulpif       = require( 'gulp-if' );
 var jshint       = require( 'gulp-jshint' );
 var notify       = require( 'gulp-notify' );
 var rename       = require( 'gulp-rename' );
 var sass         = require( 'gulp-sass' );
 var uglify       = require( 'gulp-uglify' );
 var watch        = require( 'gulp-watch' );
+var args         = require( 'yargs' ).argv;
 
 var paths = {
 	dist: 'dist/',
-	js  : ['src/float-labels.js'],
+	js  : 'src/float-labels.js',
 	scss: [
 		'demo/scss/styles.scss',
 		'src/float-labels.scss',
@@ -26,7 +29,7 @@ gulp.task( 'css', function ()
 		.src( paths.scss, { base: '.' })
 		.pipe( sass({ outputStyle: 'expanded' }).on( 'error', sass.logError ))
 		.pipe( autoprefixer() )
-		.pipe( cssnano() )
+		.pipe( gulpif( args.production, cssnano() ))
 		.pipe( rename( function( path ) {
 			path.dirname = path.dirname.replace( 'src', 'dist' );
 			path.dirname = path.dirname.replace( 'scss', 'css' );
@@ -43,7 +46,8 @@ gulp.task( 'css', function ()
  -------------------------------------------------- */
 gulp.task( 'jshint', function()
 {
-	return gulp.src( paths.js )
+	return gulp
+		.src( paths.js )
 		.pipe( jshint() )
 		.pipe( jshint.reporter( 'jshint-stylish' ))
 		.pipe( jshint.reporter( 'fail' ).on( 'error', function() { this.emit( 'end' ); }))
@@ -59,7 +63,7 @@ gulp.task( 'js', function ()
 {
 	return gulp
 		.src( paths.js )
-		.pipe( uglify({ preserveComments: 'license' }))
+		.pipe( gulpif( args.production, uglify({ preserveComments: 'license' })))
 		.pipe( rename({ suffix: '.min' }))
 		.pipe( gulp.dest( paths.dist ))
 		.pipe( browserSync.stream() )
@@ -67,6 +71,39 @@ gulp.task( 'js', function ()
 			message: 'JS Task complete!',
 			onLast : true
 		}));
+});
+
+/* Version Bump Task
+ -------------------------------------------------- */
+var bumpPaths = [
+	'bower.json',
+	'package.json',
+	'src/float-labels.js',
+	'src/float-labels.scss',
+];
+
+gulp.task( 'bump:patch', function()
+{
+	return gulp
+		.src( bumpPaths, { base: './' })
+		.pipe( bump() )
+		.pipe( gulp.dest( './' ));
+});
+
+gulp.task( 'bump:minor', function()
+{
+	return gulp
+		.src( bumpPaths, { base: './' })
+		.pipe( bump({ type: 'minor' }))
+		.pipe( gulp.dest( './' ));
+});
+
+gulp.task( 'bump:major', function()
+{
+	return gulp
+		.src( bumpPaths, { base: './' })
+		.pipe( bump({ type: 'major' }))
+		.pipe( gulp.dest( './' ));
 });
 
 /* Watch Task
