f521210d69186927d3e8ad786d7f4e4f0138c091
Nick B <nick@nickbishop.co.uk>
2018-05-25 18:30:27 +0100

    Fixes for cross-browser issues (#23)
    * Fix development dependencies
    * Listen to 'change' rather than 'input' event on IE/Edge SELECT elements
    * Fix unencoded inline SVG
    * Hide native select arrow on MS browsers

--- src/float-labels.js
+++ src/float-labels.js
@@ -169,8 +169,9 @@
 		handleEvents: function( el, action )
 		{
 			var events = this.events;
+			var isMicrosoftSelectEl = el.nodeName === 'SELECT' && this.isMSBrowser();
 			['blur','input','focus'].forEach( function( event ) {
-				if( el.type === 'file' && event === 'input' ) {
+				if( event === 'input' && ( el.type === 'file' || isMicrosoftSelectEl )) {
 					event = 'change';
 				}
 				el[ action + 'EventListener']( event, events[event] );
@@ -181,6 +182,11 @@
 		hasParent: function( el )
 		{
 			return el.parentNode.classList.contains( this.prefixed( 'wrap' ));
+		},
+
+		/** @return bool */
+		isMSBrowser: function() {
+			return !!navigator.userAgent.match(/Trident/g) || !!navigator.userAgent.match(/MSIE/g) || !!navigator.userAgent.match(/Edge/g);
 		},
 
 		/** @return bool */
