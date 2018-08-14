83fa7c65af8a166f21241bdead575be9f7e223d4
Paul Ryley <paul@geminilabs.io>
2018-08-14 10:49:10 -0600

    include unminified css

--- gulpfile.js
+++ gulpfile.js
@@ -14,60 +14,69 @@ var yaml           = require('yamljs');
 
 var config = yaml.load('config.yml');
 
-gulp.task('bump', function() {
+gulp.task('bump', function(cb) {
     var type = 'patch';
     ['prerelease','patch','minor','major'].some(function(arg) {
         if(!args[arg])return;
         type = arg;
         return true;
     });
-    return pump([
+    pump([
         gulp.src(config.bump, {base: '.'}),
-        bump({type: type, keys: ['version']}),
+        bump({type: type, keys: ['version','ver=']}),
         gulp.dest('.'),
-    ]);
+    ], cb);
 });
 
-gulp.task('js', function() {
-    return pump([
+gulp.task('css', function(cb) {
+    pump([
+        gulp.src(config.watch.css),
+        cssnano({
+            minifyFontValues: false,
+            zindex: false,
+        }),
+        rename({suffix: '.min'}),
+        gulp.dest(config.dest),
+    ], cb);
+});
+
+gulp.task('js', function(cb) {
+    pump([
         gulp.src(config.watch.js),
         uglify({
+            mangle: {properties: {regex: /_$/}},
             output: {comments: 'some'},
         }),
         rename({suffix: '.min'}),
         gulp.dest(config.dest),
         browserSync.stream(),
-    ]);
+    ], cb);
 });
 
-gulp.task('jshint', function() {
-    return pump([
+gulp.task('jshint', function(cb) {
+    pump([
         gulp.src(config.watch.js),
         jshint(),
         jshint.reporter('jshint-stylish'),
         jshint.reporter('fail'),
-    ]);
+    ], cb);
 });
 
-gulp.task('scss', function() {
-    return pump([
+gulp.task('scss', function(cb) {
+    pump([
         gulp.src(config.watch.scss, {base: '.'}),
         sass({
             importer: moduleImporter(),
             outputStyle: 'expanded',
         }).on('error', sass.logError),
         autoprefixer(),
-        cssnano({
-            minifyFontValues: false,
-            zindex: false,
-        }),
         rename(function(path) {
             path.dirname = path.dirname.replace('src','dist');
             path.dirname = path.dirname.replace('scss','css');
         }),
         gulp.dest('.'),
         browserSync.stream(),
-    ]);
+    ], cb);
 });
 
 gulp.task('watch', function() {
@@ -79,4 +88,4 @@ gulp.task('watch', function() {
     gulp.watch(config.watch.html).on('change', browserSync.reload);
 });
 
-gulp.task('default', gulp.parallel('scss','jshint','js'));
+gulp.task('default', gulp.parallel(gulp.series('scss','css'),'jshint','js'));
